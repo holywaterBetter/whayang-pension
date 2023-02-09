1a3b52c6a0870fb1f9033ff94b02058999837f06
holywaterBetter <holywater.better@gmail.com>
2023-02-09 15:03:46 +0900

    feat(#5): VideoComponent 기초 셋업

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,17 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { VideoComponent } from './video/video.component';
 
 @NgModule({
   declarations: [	
     AppComponent,
     TopBarComponent,
+    VideoComponent
    ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     MatToolbarModule,
     MatIconModule,
     MatButtonModule
