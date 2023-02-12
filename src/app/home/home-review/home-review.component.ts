71be00ebc32eaf6a2f812d2c983f304957360802
holywaterBetter <holywater.better@gmail.com>
2023-02-13 00:11:04 +0900

    feat(#6): HomeReviewComponent 좌우 스크롤 기본 구현

--- src/app/home/home-review/home-review.component.ts
+++ src/app/home/home-review/home-review.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'home-review',
@@ -6,16 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-review.component.scss']
 })
 export class HomeReviewComponent {
-
-  constructor() { }
-  isOpen: boolean;
+  hover: boolean[] = [false,false,false,false,false, false];
+  @ViewChild("reviews", {static: true}) reviews: ElementRef<HTMLElement>
+  widthReview1: number;
+  heightReview1: number;
+  constructor() {
+    console.warn("screen.width", screen.width);
+    this.widthReview1 = screen.width*0.25;
+    this.heightReview1 = screen.height * 0.6;
+  }
 
   onMouseEnter(review: number) {
-    this.isOpen = true;
+  this.hover[review] = true;
   }
 
   onMouseOut(review: number) {
-    this.isOpen = false;
+    this.hover[review] = false;
   }
 
+  move(direction: boolean){
+    if(direction){
+      this.m++;
+    }
+    else{
+      this.m--;
+    }
+    this.reviews.nativeElement.style.transform = "translate3d( calc(( (100vw - 11rem) / 3 + 2rem) * " + this.m + "), 0px, 0px)";
+  }
+  private m: number = 0;
 }
