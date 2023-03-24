eeed7ed6a19ba4b8d4bd8412f3563304dc01f9d1
holywaterBetter <holywater.better@gmail.com>
2023-03-24 15:30:31 +0900

    feat(#11) about page 구현

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  HostListener,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'about',
@@ -6,5 +13,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./about.component.scss'],
 })
 export class AboutComponent {
-  constructor() {}
+  @ViewChild('TopImage') myImg: ElementRef<HTMLImageElement>;
+  @ViewChild('Header') myText: ElementRef<HTMLDivElement>;
+
+  currentIndex: number = 0;
+  images: string[] = [
+    '/assets/pension.jpg',
+    '/assets/valley1.jpg',
+    '/assets/valley2.jpg',
+    '/assets/valley3.jpg',
+    '/assets/valley4.jpg',
+  ];
+
+  updateTextPosition() {
+    const img = this.myImg.nativeElement;
+    const height =
+      window.screen.availHeight < img.height
+        ? window.screen.availHeight
+        : img.height;
+    console.log(window.screen.availHeight, height);
+    this.myText.nativeElement.style.top = `calc(${height / 2}px)`;
+  }
+
+  onSliderThumbClick(index: number) {
+    this.currentIndex = index;
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.updateTextPosition();
+  }
 }
